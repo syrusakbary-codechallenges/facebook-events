719185c09c29c7a5bf2cdbca651f16bdce0f39f2
Syrus Akbary <sakbary@chegg.com>
2015-02-26 10:26:12 -0800

    First commit

--- src/app.js
+++
@@ -1,7 +0,0 @@
-var React = require('react');
-var EventApp = require('./components/EventApp');
-
-React.render(
-  <EventApp startHour={window.START_HOUR} endHour={window.END_HOUR} width={window.CANVAS_WIDTH} height={window.CANVAS_HEIGHT} events={window.DEFAULT_EVENTS} />,
-  document.getElementById('app')
-);
